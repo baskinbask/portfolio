30069e4350d671f9ace44a178fcbd6d875bf6f0c
Eugene <baskin.robinz@gmail.com>
2018-02-05 02:40:51 +0300

    first time files upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    // Попап
+    $(function() {
+        //----- OPEN
+        $('[data-popup-open]').on('click', function(e) {
+            var targeted_popup_class = jQuery(this).attr('data-popup-open');
+            $('[data-popup="' + targeted_popup_class + '"]').bPopup({
+                position: [0, 20]
+            });
+
+
+            e.preventDefault();
+        });
+    });
+
+    // Открываем ссылки в новой вкладке
+    $.fn.replaceUrl = function() {
+            var regexp = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+            this.each(function() {
+                $(this).html(
+                    $(this).html().replace(regexp, '<a href="$1" target="windowName">$1</a>'));
+            });
+            return $(this);
+        }
+    $('.popup').replaceUrl();
+
+    // Filter Фильтор
+    //You can override any of these options and then call...
+    $('.portfolio-list').mixItUp({
+        animation: {
+            effects: 'fade stagger(10ms)',
+            staggerSequence: function(i){
+                return (2*i) - (5*((i/3) - ((1/3) * (i%3))));
+            },
+            queueLimit: 2
+        }
+    });
+
+
+});
